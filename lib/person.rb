e82d8eeae08bf8bd34532baaf7e6eb94f1dc884e
Yigit Mersin <yigitmersin@gmail.com>
2022-03-04 08:55:47 +0300

    Add find_rental by id feature

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
 
   def initialize(age, name = 'unknown', parent_permission = false, *)
     super()
-    @id = rand(0..1_000_000)
+    @id = rand(8..100)
     @age = age
     @name = name
     @parent_permission = parent_permission
