2167605180b825bc0bcbde828c189ccc3c0dab9c
Yigit Mersin <yigitmersin@gmail.com>
2022-03-08 19:13:37 +0300

    Add book read & write data from file method

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Book
   attr_accessor :title, :author, :rentals
 
@@ -7,8 +9,29 @@ class Book
     @rentals = []
   end
 
+  @@path = "data/book.json"
+
   def add_rental(rental)
     @rentals = rental
     rental.book = self
+  end
+
+  def self.read_file
+    JSON.parse(File.read(@path)).each do |element|
+      p(element)
+    end
+  end
+
+  def self.check_file
+    File.exist?(@path) ? true : false
+  end
+
+  def self.write_file(data = [])
+    data_arr = []
+    data.each do |d|
+      data_arr << {title: d.title, author: d.author}
+    end
+    p(data_arr)
+    File.write(@@path, JSON.generate(data_arr))
   end
 end
